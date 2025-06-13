97bca5402ebcae813f1594cf77a275a09a5d9563
Admin <admin@jaccxsoftware.pe>
2025-06-13 02:25:31 -0500

    update apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://44.197.238.25/api/productos' // URL de desarrollo
+    apiUrl: 'https://hmlek9rkbg.execute-api.us-east-1.amazonaws.com/dev/api/productos' // URL de desarrollo
   };
   
