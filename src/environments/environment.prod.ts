87ee2935cad63601bfb88d314f613c92ede498ca
Admin <admin@jaccxsoftware.pe>
2025-06-13 03:38:24 -0500

    update apiUrl v3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://hmlek9rkbg.execute-api.us-east-1.amazonaws.com/dev/api/productos' // URL de producción
+    apiUrl: 'https://ftpjzsvxh7.execute-api.us-east-1.amazonaws.com/dev/api/productos' // URL de producción
   };
   
